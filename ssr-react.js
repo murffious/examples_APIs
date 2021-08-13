2d7fff43dc3b57c936731ffdaa20481d1abf9c87
Paul Murff <25558342+murffious@users.noreply.github.com>
2021-08-13 13:54:29 -0600

    Update ssr-react.js

--- ssr-react.js
+++ ssr-react.js
@@ -1,1 +1,4 @@
 
+![Screen Shot 2021-08-12 at 11 05 44 AM](https://user-images.githubusercontent.com/25558342/129411762-7afaa3ef-b63b-4970-a630-1292bf272422.png)
+<img width="1191" alt="Screen Shot 2021-08-13 at 11 02 29 AM" src="https://user-images.githubusercontent.com/25558342/129411766-2f51bee9-a658-41fd-90b1-643a21a35222.png">
+<img width="1092" alt="Screen Shot 2021-08-13 at 11 25 44 AM" src="https://user-images.githubusercontent.com/25558342/129411769-69858594-6473-4de0-8545-6db53b2c803c.png">
